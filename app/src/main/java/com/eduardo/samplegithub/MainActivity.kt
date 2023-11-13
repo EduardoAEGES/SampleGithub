2e57468b3fa07e44817708482acc6edfe5a53e86
contacat <edu.mamaniroque@gmail.com>
2023-11-13 03:14:02 -0500

    FirstMod

--- app/src/main/java/com/eduardo/samplegithub/MainActivity.kt
+++ app/src/main/java/com/eduardo/samplegithub/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.eduardo.samplegithub.ui.theme.SampleGithubTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    //Función a llamar
                     Greeting("Android")
                 }
             }
@@ -32,8 +34,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hola $name bienvenido!",
+        modifier = modifier,
+        fontSize = 50.sp
     )
 }
 
